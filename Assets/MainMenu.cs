3a6037a2c7bd4b5ed93341446943ba80ee7ebd92
AlexUji <90849122+AlexUji@users.noreply.github.com>
2023-10-26 13:26:48 +0200

    Add files via upload

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void EscenaJuego()
+    {
+        SceneManager.LoadScene("1.Plaza");
+
+    }
+
+    public void EscenaOpciones()
+    {
+        SceneManager.LoadScene("Opciones");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
